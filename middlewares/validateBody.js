c9a237750b527a349772c0f3386690f719391739
Tasmyt <121162710+Tasmyt@users.noreply.github.com>
2023-08-19 08:54:36 +0300

    Переробила Joi

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -10,16 +10,6 @@ const validateBody = (schema) => {
       if (!name && !email && !phone) {
         res.status(400).json({ message: `Missing fields` });
       }
-      if (!name) {
-        res.status(400).json({ message: `Missing required name field` });
-      }
-      if (!email) {
-        res.status(400).json({ message: `Missing required email field` });
-      }
-      if (!phone) {
-        res.status(400).json({ message: `Missing required phone field` });
-      }
-
       next(httpError(400, error.message));
     }
     next();
